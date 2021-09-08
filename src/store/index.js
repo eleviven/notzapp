faf779729ee904b34f4355eeaf135fb4b6607d65
eleviven <firuz.haciyeff@gmail.com>
2021-09-08 14:11:11 +0400

    state persist with local storage

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,23 @@
 import { configureStore } from "@reduxjs/toolkit";
+
 import notesSlice from "./slices/notes.slice.js";
 import writeBoxSlice from "./slices/write-box.slice.js";
+
+import {
+  getReduxStateFromStorage,
+  setReduxStateToStorage,
+} from "../services/store.service.js";
+
+const preloadedState = getReduxStateFromStorage();
 
 export const store = configureStore({
   reducer: {
     notes: notesSlice,
     writeBox: writeBoxSlice,
   },
+  preloadedState,
+});
+
+store.subscribe(() => {
+  setReduxStateToStorage(store.getState());
 });
