4ea142d867286b9e5bb8caff63a9f1f93ba8ea85
eleviven <firuz.haciyeff@gmail.com>
2021-09-08 14:09:40 +0400

    set initial color

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,20 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { Box } from "@chakra-ui/layout";
 import { WriteBox } from "../components";
 import { NoteList } from "../containers";
+import { colorSelector, setColor } from "../store/slices/write-box.slice";
+import data from "../static/data";
 
 export default function Home() {
+  const color = useSelector(colorSelector);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (!color) {
+      dispatch(setColor(data.colors[0]));
+    }
+  }, [dispatch, color]);
   return (
     <Box>
       <WriteBox />
