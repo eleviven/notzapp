93632d5001dec31bb3dc94a15e8a9d63a9a46368
eleviven <firuz.haciyeff@gmail.com>
2021-09-13 02:54:54 +0400

    Layout modified, remove container

--- src/components/layout/layout.js
+++ src/components/layout/layout.js
@@ -1,24 +1,17 @@
 import React from "react";
-import { useSelector } from "react-redux";
-import { Box, Container } from "@chakra-ui/react";
+import { Box } from "@chakra-ui/react";
 import { Header } from "../";
-import { activeColorSelector } from "../../store/slices/write-box.slice";
 
 export default function Layout({ children }) {
-  const color = useSelector(activeColorSelector);
   return (
-    <Box backgroundColor={color.backgroundColor}>
-      <Container maxW="container.md" paddingX={[0, 0, 15]}>
-        <Box
-          border="1px"
-          borderColor="gray.200"
-          backgroundColor="white"
-          minH="100vh"
-        >
-          <Header />
-          {children}
-        </Box>
-      </Container>
+    <Box
+      border="1px"
+      borderColor="gray.200"
+      backgroundColor="white"
+      minH="100vh"
+    >
+      <Header />
+      {children}
     </Box>
   );
 }
