1286500ca224d3b8ea9c2e0074e93e7fadd41e42
eleviven <firuz.haciyeff@gmail.com>
2021-09-13 02:53:48 +0400

    Add active note state to edit

--- src/components/write-box/write-box-text-area.js
+++ src/components/write-box/write-box-text-area.js
@@ -1,9 +1,16 @@
 import { Textarea } from "@chakra-ui/react";
+import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { setText, writeBoxSelector } from "../../store/slices/write-box.slice";
+import { activeNoteSelector } from "../../store/slices/notes.slice";
+import {
+  reset,
+  setText,
+  writeBoxSelector,
+} from "../../store/slices/write-box.slice";
 
 export default function WriteBoxTextArea() {
   const { text } = useSelector(writeBoxSelector);
+  const activeNote = useSelector(activeNoteSelector);
   const dispatch = useDispatch();
 
   const handleChange = (e) => {
@@ -11,14 +18,24 @@ export default function WriteBoxTextArea() {
     dispatch(setText(value));
   };
 
+  useEffect(() => {
+    if (activeNote) {
+      dispatch(setText(activeNote.text));
+    }
+    return () => {
+      dispatch(reset());
+    };
+  }, [dispatch, activeNote]);
+
   return (
     <Textarea
       placeholder="Enter your note here"
       borderRadius={0}
       borderWidth={0}
       resize="vertical"
-      rows="3"
+      rows="8"
       value={text}
+      autoFocus={true}
       onChange={handleChange}
     />
   );
