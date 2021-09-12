1fcfc89e76575349a25cf58e8cafcb114d330efe
eleviven <firuz.haciyeff@gmail.com>
2021-09-13 02:52:42 +0400

    Add active note id and selector

--- src/store/slices/notes.slice.js
+++ src/store/slices/notes.slice.js
@@ -1,11 +1,18 @@
-import { createSlice, createEntityAdapter, nanoid } from "@reduxjs/toolkit";
+import {
+  createSlice,
+  createEntityAdapter,
+  nanoid,
+  createSelector,
+} from "@reduxjs/toolkit";
 
 const notesAdapter = createEntityAdapter({
   sortComparer: (a, b) => new Date(b.createdAt) - new Date(a.createdAt),
 });
 export const notesSelector = notesAdapter.getSelectors((state) => state.notes);
 
-const initialState = notesAdapter.getInitialState();
+const initialState = notesAdapter.getInitialState({
+  activeNoteId: null,
+});
 
 const notesSlice = createSlice({
   name: "notes",
@@ -23,10 +30,25 @@ const notesSlice = createSlice({
         },
       }),
     },
+    updateNote: notesAdapter.updateOne,
     removeNote: notesAdapter.removeOne,
+    setActiveNoteId: (state, action) => {
+      state.activeNoteId = action.payload;
+    },
   },
 });
 
-export const { addNote, removeNote } = notesSlice.actions;
+export const activeNoteSelector = createSelector(
+  (state) => state.notes,
+  ({ activeNoteId, entities }) => {
+    if (activeNoteId in entities) {
+      return entities[activeNoteId];
+    }
+    return null;
+  }
+);
+
+export const { addNote, updateNote, removeNote, setActiveNoteId } =
+  notesSlice.actions;
 
 export default notesSlice.reducer;
